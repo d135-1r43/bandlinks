c223c44dd597b7f68eff8a47dbdd6e3c4b586c08
mhe <markus.herhoffer@exentra.de>
2024-01-07 13:12:16 +0100

    🌱 (legacy): Add scaffolding from legacy implementation with DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-screen" style="background-image: url(img/hero-bg-01.jpg);">
+	<div class="hero-overlay bg-opacity-20"></div>
+	<div class="hero-content text-center text-neutral-content">
+		<div class="max-w-md">
+			<h1><img class="mb-5 mt-15" src="img/hero-logo.png" alt="Opus Irae"/></h1>
+			<p class="mb-20 text-2xl tracking-widest uppercase font-thin">Symphonic Black Metal</p>
+
+			<a class="btn btn-block mb-5 opacity-75 shadow-2xl shadow-inner" href="https://www.instagram.com/opusirae/">
+				<i class="fa-brands fa-instagram mr-4 text-2xl"></i>
+				<span class="tracking-widest">Instagram</span>
+			</a>
+			<a class="btn btn-block mb-5 opacity-75 shadow-2xl shadow-inner" href="https://www.facebook.com/opusirae">
+				<i class="fa-brands fa-facebook mr-4 text-2xl"></i>
+				<span class="tracking-widest">Facebook</span>
+			</a>
+			<a class="btn btn-block mb-5 opacity-75 shadow-2xl shadow-inner" href="https://opus-irae.com">
+				<i class="fa-solid fa-newspaper mr-4 text-2xl"></i>
+				<span class="tracking-widest">Homepage</span>
+			</a>
+			<a class="btn btn-block mb-5 opacity-75 shadow-2xl shadow-inner" href="https://twitter.com/opusirae">
+				<i class="fa-brands fa-twitter mr-4 text-2xl"></i>
+				<span class="tracking-widest">Twitter</span>
+			</a>
+			<a class="btn btn-block mb-5 opacity-75 shadow-2xl shadow-inner" href="https://opusirae.bandcamp.com">
+				<i class="fa-brands fa-bandcamp mr-4 text-2xl"></i>
+				<span class="tracking-widest">Bandcamp</span>
+			</a>
+			<a class="btn btn-block mb-5 opacity-75 shadow-2xl shadow-inner"
+				 href="https://open.spotify.com/artist/3AFQQ6CRoysoxWfUGjnosX">
+				<i class="fa-brands fa-spotify mr-4 text-2xl"></i>
+				<span class="tracking-widest">Spotify</span>
+			</a>
+			<a class="btn btn-block mb-5 opacity-75 shadow-2xl shadow-inner"
+				 href="https://itunes.apple.com/de/artist/opus-irae/1063888113">
+				<i class="fa-brands fa-apple mr-4 text-2xl"></i>
+				<span class="tracking-widest">Apple Music</span>
+			</a>
+
+		</div>
+	</div>
+</div>
+<footer class="footer items-center p-4 bg-neutral text-neutral-content">
+	<div class="items-center grid-flow-col">
+		<p>© 2022</p>
+	</div>
+	<div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+		Jurisdiction: Germany
+		<a href="http://opus-irae.com/impressum/">
+			<span>Impressum & Datenschutzerklärung</span>
+		</a>
+	</div>
+</footer>
