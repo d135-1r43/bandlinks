692a80a92b9d8c6057e017f70b03c1c5bbbf48c9
Markus Herhoffer <markus@herhoffer.net>
2025-09-21 12:49:53 +0200

    🛠️ (config): Comment out daisyUI configurations in Tailwind for upgrade
    Temporarily disabled daisyUI plugin and its import in `tailwind.config.js`. This change avoids potential compatibility issues while maintaining the base configuration intact.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
-import daisyui from 'daisyui';
+//import daisyui from 'daisyui';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: [daisyui]
+	//plugins: [daisyui]
 };
 
